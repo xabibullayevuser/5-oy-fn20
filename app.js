72342a51f22abce20411e60f316872816b2b4377
xabibullayevuser <xusniddinxabibullayev25@gmail.com>
2024-03-31 15:31:24 +0500

    ERP

---
+++ app.js
@@ -0,0 +1,42 @@
+//1-masala
+
+// class Cars {
+//     constructor(brand, year) {
+//       this.brand = brand;
+//       this.year = year;
+//     }
+//   }
+ 
+//   const car1 = new Cars("Mercedes-Benz AMG GT63S", 2023);
+//   const car2 = new Cars("BMW M5 CS", 2021);
+//   const car3 = new Cars("PORSCHE 911", 2024);
+  
+//   console.log(car1.brand, car1.year); 
+//   console.log(car2.brand, car2.year); 
+//   console.log(car3.brand, car3.year); 
+
+// 2-masala
+// class Car {
+//     constructor(brand, year) {
+//       this.brand = brand;
+//       this.year = year;
+//     }
+//   }
+  
+//   class powerBank extends Car {
+//     constructor(brand, year, batteryLife) {
+//       super(brand, year);  
+//       this.batteryLife = batteryLife;
+//     }
+//   }
+  
+//   const car1 = new Car("BMW I7", 2024);
+//   const powerBank1 = new powerBank("Tesla MODEL S", 2021);
+
+//   console.log(car1.brand, car1.year);
+//   console.log(powerBank1.brand, powerBank1.year, powerBank1.batteryLife); 
+
+//3-masala
+
+  
+  
